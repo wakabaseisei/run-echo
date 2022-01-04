7e4bf655fa650874d5097e9ea314d5bf2ef2e247
wakabayashi <wakabaseisei@gmail.com>
2022-01-05 00:40:55 +0900

    NodePortで公開して、GoAPIからUserからのリクエストがJSONで返ってくる

--- src/infrastracture/Config.go
+++ src/infrastracture/Config.go
@@ -24,12 +24,12 @@ func NewConfig() *Config {
 
 	c := new(Config)
 
-	c.DB.Production.Host = "mysql"
+	c.DB.Production.Host = "mysql-server"
 	c.DB.Production.Username = "go_user"
 	c.DB.Production.Password = "password"
 	c.DB.Production.DBName = "go_database"
 
-	c.DB.Test.Host = "mysql"
+	c.DB.Test.Host = "mysql-server"
 	c.DB.Test.Username = "go_user"
 	c.DB.Test.Password = "password"
 	c.DB.Test.DBName = "go_database"
